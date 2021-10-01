f81ef4e63c6c20f3b6ffad37a66c403b94822eab
Mihai Dornescu <dornescu@gmail.com>
2021-10-01 18:32:39 +0300

    controllers

---
+++ controllers/routes-controllers.js
@@ -0,0 +1,47 @@
+const HttpError = require("../model/http-error.js");
+
+const test = [
+	{
+		id: '1',
+		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+		category: 'nodeJs',
+		title: 'routing'
+	},
+	{
+		id: '2',
+		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+		category: 'nodeJs',
+		title: 'error'
+	},
+	{
+		id: '3',
+		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+		category: 'nodeJs',
+		title: 'middleware'
+	},
+	{
+		id: '4',
+		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+		category: 'css',
+		title: 'middleware'
+	}
+];
+
+
+const getNodeRoutesById =(req, res, next) => {
+	const routeId = req.params.pid;
+	const filterItem = test.find(el => el.id === routeId);
+	if(!filterItem){
+		throw new HttpError('could not find this id...', 404);
+	}
+	res.status(200).json({message: filterItem});
+};
+const getAllNodeRoutes =(req, res, next) => {
+	res.status(200).json(test);
+};
+// o varianta!!
+// module.exports = getNodeRoutesById;
+// module.exports = getAllNodeRoutes;
+
+exports.getNodeRoutesById = getNodeRoutesById;
+exports.getAllNodeRoutes = getAllNodeRoutes;
--- routes/nodeJs.js
+++ routes/nodeJs.js
@@ -1,59 +1,23 @@
 const express = require('express');
 const HttpError = require('../model/http-error.js');
 
-const test = [
-	{
-		id: '1',
-		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		category: 'nodeJs',
-		title: 'routing'
-	},
-	{
-		id: '2',
-		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-		category: 'nodeJs',
-		title: 'error'
-	},
-	{
-		id: '3',
-		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-		category: 'nodeJs',
-		title: 'middleware'
-	},
-	{
-		id: '4',
-		desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-		category: 'css',
-		title: 'middleware'
-	}
-];
+const routesControllers = require('../controllers/routes-controllers');
 
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	res.status(200).json(test);
-});
+router.get('/', routesControllers.getAllNodeRoutes);
 
-router.get('/:pid', (req, res, next) => {
-	const routeId = req.params.pid;
-	const filterItem = test.find(el => el.id === routeId);
-
-	// if (!filterItem) {
-	// 	return res.status(404).json({message: `no route with this id: ${routeId}`});
-	// }
-	// if(!filterItem){
-	// 	const error = new Error('could not find ...');
-	// 	error.code= 404;
-	// 	throw error;
-	// }
-	if(!filterItem){
-		throw new HttpError('could not find this id...', 404);
-	}
-
-	res.status(200).json({message: filterItem});
-
-});
+router.get('/:pid', routesControllers.getNodeRoutesById);
 
 
 module.exports = router;
+
+// if (!filterItem) {
+// 	return res.status(404).json({message: `no route with this id: ${routeId}`});
+// }
+// if(!filterItem){
+// 	const error = new Error('could not find ...');
+// 	error.code= 404;
+// 	throw error;
+// }
